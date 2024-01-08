8d943f026bcbd441ac2722293dc46c6f73579635
lalmastar <107454522+lalmastar@users.noreply.github.com>
2024-01-08 16:01:53 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   console.log("DB Connected Successfully")
-    mongoose.connect("mongodb+srv://lalmastar12:Alex321@getdoctors.rmq6wj7.mongodb.net/?retryWrites=true&w=majority", {
+    mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     // useCreateIndex: true
 })
